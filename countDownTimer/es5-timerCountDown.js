be622816fd9bf101d53947f6a3d1ff6cc3d7455f
hexisen <616082046@qq.com>
2018-03-20 14:55:46 +0800

    add es5 timerCountDown

---
+++ countDownTimer/es5-timerCountDown.js
@@ -0,0 +1,55 @@
+/*
+ * @Author: Mr.He 
+ * @Date: 2018-03-19 21:29:59 
+ * @Last Modified by: Mr.He
+ * @Last Modified time: 2018-03-20 14:52:49
+ * @content what is the content of this file. */
+
+var timerCountDown = {
+    computeTimer: function (number) {
+        var data = {
+            days: 0,
+            hours: 0,
+            minutes: 0,
+            seconds: 0
+        };
+        data.days = Math.floor(number / (60 * 60 * 24));
+        data.hours = Math.floor(number % (60 * 60 * 24) / (60 * 60));
+        data.minutes = Math.floor(number % (60 * 60) / 60);
+        data.seconds = number % 60;
+
+        for (var key in data) {
+            data[key] = this.format(data[key]);
+        }
+        return data;
+    },
+    changeDom: function (obj) {
+        var str = `${obj.days}天 ${obj.hours}小时 ${obj.minutes}分钟 ${obj.seconds}秒`;
+        document.getElementById("timer").innerHTML = str;
+        console.log(str);
+    },
+    format: function (str) {
+        str = String(str);
+        return str.length < 2 ? '0' + str : str;
+    },
+    timer: null,
+
+    /* number 单位秒 */
+    init: function (number, finishFn, viewChange) {
+        var self = this;
+        this.timer = setInterval(function () {
+            if (number < 0) {
+                self.timer = clearInterval(self.timer);
+                return finishFn && finishFn();
+            }
+            var timerData = self.computeTimer(number);
+            number--;
+
+            return viewChange ? viewChange(timerData) : self.changeDom(timerData);
+        }, 50);
+    }
+}
+
+timerCountDown.init(100, () => {
+    console.log("timer down")
+});
